3a8bc0057caef2654a3236b87ef63b21a4484464
Deepeshgodhwani <deepesh.godhwani@techolution.com>
2024-08-20 14:21:42 +0530

    integrated appwrite in next app

---
+++ src/config/config.ts
@@ -0,0 +1,7 @@
+const config = {
+    appwritePublicUrl: String(process.env.NEXT_PUBLIC_APPWRITE_URL),
+    appwriteProjectId: String(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID)
+}
+
+
+export default config;
